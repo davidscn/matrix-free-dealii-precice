58d570686b8505cad1f9766dafbc750dcd636812
David Schneider <dav.schneider@tum.de>
2021-01-13 09:03:02 +0100

    Update comment in case template

--- include/cases/case_template.h
+++ include/cases/case_template.h
@@ -22,11 +22,13 @@ namespace FSI
     {
       // Create your coarse mesh here
       // make sure you use this->interface_id for the coupling interface when
-      // distributing the boundary IDs
+      // distributing the boundary IDs.
+      // Dirichlet boundary conditions are applied through this->dirichlet and
+      // this->dirichlet_mask. Have a look at other cases.
 
-      // material_id is used for the default material
       // material_id 2 is currently used for the inclusion material, which is
-      // 100 x stiffer (mu) than the usual material
+      // 100 x stiffer (mu) than the usual material. Other IDs are used for the
+      // default material
     }
   } // namespace TestCases
 } // namespace FSI
