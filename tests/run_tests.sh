5dbeb3ef372865f7315a6b2627863e8ba647a609
David Schneider <dav.schneider@tum.de>
2021-01-13 14:28:20 +0100

    Add a basic test infrastructure

---
+++ tests/run_tests.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+echo "Running tests for matrix-free-dealii-precice"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+
+# Run from this directory
+cd ${0%/*} || exit 1
+
+declare -a arr=("turek_gmg_scalar_referential" "turek_gmg_tensor2" "turek_gmg_tensor4" "turek_gmg_tensor4_ns" "turek_gmg_scalar" "turek_gmg_scalar_form0" )
+
+exit_code=0
+
+test_dir=$(pwd)
+
+print_start(){
+	echo -e " ***Test $@:"
+	echo -ne "\t Starting ..."
+}
+
+print_result() {
+	if [ $? -eq 0 ]
+	then
+            echo -ne "${GREEN} passed ${NOCOLOR}\n"
+	else
+            echo -ne "${RED} failed ${NOCOLOR}\n"
+            exit_code=$[$exit_code +1]
+            cat $@/$@.diff
+	fi
+}
+
+test_name="building"
+print_start ${test_name}
+mkdir -p build && cd build
+(cmake ../../ && make debug) &>${test_name}.log
+print_result ${test_name}
+cd ..
+cp ./build/solid ./dummy_tester
+
+echo "Building the tester..."
+cd ./dummy_tester
+(cmake . && make) &> tester-build.log
+
+# Specify the precice-config
+ln -sf precice-config_unified.xml precice-config.xml
+for i in "${arr[@]}"
+    do
+    test_name=$i
+    print_start ${test_name}
+    ./solid $i/$i.prm&> $i/$i.log & ./dummy_tester &>$i/tester.log
+    numdiff  $i/output $i/${test_name}.output &>$i/${test_name}.diff
+print_result ${test_name}
+done
+
+if [ $exit_code -eq 0 ]
+then
+    echo "All tests passed."
+else
+    echo "Errors occurred: $exit_code tests failed."
+    exit 1
+fi
