5ea580463ea78bbeec3bdae1fa0b8deaa588e611
DavidSCN <david.schneider@ipvs.uni-stuttgart.de>
2021-04-06 11:36:44 +0200

    Implement merged triangulation for square pillar

--- include/cases/Wall_beam.h
+++ include/cases/Wall_beam.h
@@ -29,49 +29,57 @@ namespace FSI
     WallBeam<dim>::make_coarse_grid_and_bcs(Triangulation<dim> &triangulation)
     {
       AssertDimension(dim, 2);
-      // Select initially a very coarse mesh
-      uint n_x = 5;
-      uint n_y = 1;
-      uint n_z = 1;
+      // tria1 contains the beam, tria2 and tria3 contain the square
+      Triangulation<dim> tria1, tria2, tria3;
 
+      // We need five subdivisions in x-direction
       const std::vector<unsigned int> repetitions =
-        dim == 2 ? std::vector<unsigned int>({n_x, n_y}) :
-                   std::vector<unsigned int>({n_x, n_y, n_z});
+        dim == 2 ? std::vector<unsigned int>({5, 1}) :
+                   std::vector<unsigned int>({5, 1, 1});
 
+      // Create a beam through the square
       const Point<dim> bottom_left =
-        (dim == 3 ? Point<dim>(0.0, -0.03, -0.005) : Point<dim>(0.0, -0.03));
+        (dim == 3 ? Point<dim>(-1.0, -0.03, -0.005) : Point<dim>(-1.0, -0.03));
       const Point<dim> top_right =
         dim == 3 ? Point<dim>(4.0, 0.03, 0.005) : Point<dim>(4.0, 0.03);
 
-      GridGenerator::subdivided_hyper_rectangle(triangulation,
+      // Boundary indicators are lost during the subsequent merge
+      GridGenerator::subdivided_hyper_rectangle(tria1,
                                                 repetitions,
                                                 bottom_left,
                                                 top_right,
-                                                /*colorize*/ true);
-      const types::boundary_id clamped_mesh_id              = 1;
-      const types::boundary_id out_of_plane_clamped_mesh_id = 2;
+                                                /*colorize*/ false);
+
+      {
+        // Create the bottom part of the square
+        const Point<dim> bottom_square =
+          (dim == 3 ? Point<dim>(-1.0, -0.5, -0.005) : Point<dim>(-1.0, -0.5));
+        const Point<dim> top_square =
+          dim == 3 ? Point<dim>(0.0, -0.03, 0.005) : Point<dim>(0.0, -0.03);
+
+        GridGenerator::hyper_rectangle(tria2, bottom_square, top_square);
+      }
+      {
+        // Create the top part of the square
+        const Point<dim> bottom_square =
+          (dim == 3 ? Point<dim>(-1.0, 0.03, -0.005) : Point<dim>(-1.0, 0.03));
+        const Point<dim> top_square =
+          dim == 3 ? Point<dim>(0.0, 0.5, 0.005) : Point<dim>(0.0, 0.5);
+
+        GridGenerator::hyper_rectangle(tria3, bottom_square, top_square);
+      }
+
+      // Merge everything together
+      GridGenerator::merge_triangulations({&tria1, &tria2, &tria3},
+                                          triangulation);
+
+      const types::boundary_id clamped_mesh_id = 1;
 
       // Set boundary conditions
       // Fix all boundary components
       this->dirichlet_mask[clamped_mesh_id] = ComponentMask(dim, true);
       this->dirichlet[clamped_mesh_id] =
         std::make_unique<Functions::ZeroFunction<dim>>(dim);
-      // In case we run a 3D case, only the z component is fixed
-      if (dim == 3)
-        {
-          this->dirichlet_mask[out_of_plane_clamped_mesh_id] =
-            ComponentMask({false /*x*/, false /*y*/, true /*z*/});
-          this->dirichlet[out_of_plane_clamped_mesh_id] =
-            std::make_unique<Functions::ZeroFunction<dim>>(dim);
-        }
-
-      // boundary IDs are obtained through colorized = true
-      uint id_flap_short_bottom        = 0; // x direction
-      uint id_flap_short_top           = 1;
-      uint id_flap_long_bottom         = 2; // y direction
-      uint id_flap_long_top            = 3;
-      uint id_flap_out_of_plane_bottom = 4; // z direction
-      uint id_flap_out_of_plane_top    = 5;
 
       // Iterate over all cells and set the IDs
       for (const auto &cell : triangulation.active_cell_iterators())
@@ -79,19 +87,13 @@ namespace FSI
           for (const auto &face : cell->face_iterators())
             if (face->at_boundary() == true)
               {
+                const auto center = face->center();
                 // Boundaries for the interface
-                if (face->boundary_id() == id_flap_short_top ||
-                    face->boundary_id() == id_flap_long_bottom ||
-                    face->boundary_id() == id_flap_long_top)
+                if (center[0] > 0.0)
                   face->set_boundary_id(this->interface_id);
                 // Boundaries clamped in all directions
-                else if (face->boundary_id() == id_flap_short_bottom)
+                else
                   face->set_boundary_id(clamped_mesh_id);
-                // Boundaries clamped out-of-plane (z) direction
-                else if (face->boundary_id() == id_flap_out_of_plane_bottom ||
-                         face->boundary_id() == id_flap_out_of_plane_top)
-                  face->set_boundary_id(out_of_plane_clamped_mesh_id);
-                // on the coarse mesh reset material ID
               }
           cell->set_material_id(0);
         }
