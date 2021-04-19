a8c69e23e9593baa22dac731db08e0bc33c359e9
DavidSCN <david.schneider@ipvs.uni-stuttgart.de>
2021-04-19 10:29:18 +0200

    Remove shared library target

--- src/mf_elasticity.cc
+++
@@ -1,7 +0,0 @@
-#include <mf_elasticity.h>
-#include <template_list.h>
-
-// TODO: Find a suitable split of instantiations and move code in the source
-// files
-// explicit instantiations
-// template class FSI::Solid<2, 1, 2, double>;
