454d63e9e9f61b408bf9d2bf0f9c757ad9babaaf
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2024-02-20 13:39:32 +0100

    Add geometric setup

--- include/cases/elasticity/tendon.h
+++ include/cases/elasticity/tendon.h
@@ -32,12 +32,12 @@ namespace TestCases
 
     // Select initially a very coarse mesh
     uint       n_x = 1;
-    uint       n_y = 2;
-    uint       n_z = 3;
+    uint       n_y = 1;
+    uint       n_z = 1;
     Point<dim> point_bottom =
-      dim == 3 ? Point<dim>(0.5, 0.0, -0.3) : Point<dim>(0.5, 0);
+      dim == 3 ? Point<dim>(0.0, 0.0, 15) : Point<dim>(0, 0);
     Point<dim> point_tip =
-      dim == 3 ? Point<dim>(0.57, 0.35, 0.3) : Point<dim>(0.57, 0.35);
+      dim == 3 ? Point<dim>(3.0, 3.0, 20) : Point<dim>(0.0, 0.0);
 
     const std::vector<unsigned int> repetitions =
       dim == 3 ? std::vector<unsigned int>({n_x, n_y, n_z}) :
@@ -51,7 +51,7 @@ namespace TestCases
                                               /*colorize*/ false);
 
     // NOTE: we need to set IDs regardless of cell being locally owned or
-    // not as in the global refinement cells will be repartitioned and faces
+    // not as in the g lobal refinement cells will be repartitioned and faces
     // of their parents should have right IDs
     const unsigned int clamped_mesh_id = 1;
     const unsigned int do_nothing_id   = 2;
@@ -70,10 +70,10 @@ namespace TestCases
           if (face->at_boundary() == true)
             {
               // The left coupling interface
-              if (std::abs(face->center()[0] - point_bottom[0]) < 1e-6)
+              if (std::abs(face->center()[2] - point_bottom[2]) < 1e-6)
                 face->set_boundary_id(this->interfaces[0]);
               // The right coupling interface
-              else if (std::abs(face->center()[0] - point_tip[0]) < 1e-6)
+              else if (std::abs(face->center()[2] - point_tip[2]) < 1e-6)
                 face->set_boundary_id(this->interfaces[1]);
               else
                 face->set_boundary_id(do_nothing_id);
@@ -93,11 +93,14 @@ namespace TestCases
     const unsigned int  n_refinement_levels) const
   {
     triangulation.refine_global(n_refinement_levels);
+    const double z_height = 5;
+    const double z_height_per_cell =
+      z_height / std::pow(2, n_refinement_levels);
 
     // TODO: Make this dependent on the refinement level
     // you can use the information from the triangulation to do so
-    const double       lower_x         = -1;
-    const double       upper_x         = -1;
+    const double       lower_x = (z_height / 2) - (z_height_per_cell / 2);
+    const double       upper_x = (z_height / 2) + (z_height_per_cell / 2);
     const unsigned int clamped_mesh_id = 1;
 
     bool any_face_is_clamped = false;
@@ -108,8 +111,8 @@ namespace TestCases
           if (face->at_boundary() == true)
             {
               // The left coupling interface
-              if (face->center()[0] - lower_x > 1e-6 &&
-                  face->center()[0] - upper_x < 1e-6)
+              if (face->center()[2] - lower_x > 1e-6 &&
+                  face->center()[2] - upper_x < 1e-6)
                 {
                   face->set_boundary_id(clamped_mesh_id);
                   any_face_is_clamped = true;
